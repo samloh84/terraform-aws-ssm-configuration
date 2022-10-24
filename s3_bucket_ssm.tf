75c84be665de996619a5960ef64f1f90d3b0d708
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2022-10-24 08:46:57 +0800

    Fixing S3 bucket configurations
    Signed-off-by: Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>

--- s3_bucket_ssm.tf
+++ s3_bucket_ssm.tf
@@ -4,12 +4,12 @@ resource "aws_s3_bucket" "ssm_logs_s3_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "ssm_logs_s3_bucket_acl" {
-  bucket = aws_s3_bucket.ssm_logs_s3_bucket.arn
+  bucket = aws_s3_bucket.ssm_logs_s3_bucket.bucket
   acl    = "private"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "ssm_logs_s3_bucket_sse_config" {
-  bucket = aws_s3_bucket.ssm_logs_s3_bucket.arn
+  bucket = aws_s3_bucket.ssm_logs_s3_bucket.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
